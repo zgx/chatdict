0f29b530182ac51d6efcbfdf83aa82904ee4e85a
zgx <gxzeng007@gmail.com>
2022-10-18 14:58:20 +0800

    website info

---
+++ webapi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace chatdict.webapi.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HomeController: ControllerBase
+{
+    public class Website
+    {
+        public string Name { get; } = "chatdict";
+        public string Version { get; } = "v1";
+    }
+    
+    [HttpGet]
+    public Website Get()
+    {
+        return new Website();
+    }
+}
