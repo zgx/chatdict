b6a8aca719fa863a886506d661efb8c89f1b2d34
zgx <gxzeng007@gmail.com>
2022-10-19 17:14:03 +0800

    AddNewtonsoftJson

--- webapi/Controllers/TelHookController.cs
+++ webapi/Controllers/TelHookController.cs
@@ -1,10 +1,9 @@
-﻿using System.Text.Json.Serialization;
-using chatdict.webapi.Service;
-using Telegram.Bot;
+﻿
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using Telegram.Bot;
+using Newtonsoft.Json.Linq;
 using Telegram.Bot.Types;
-using Microsoft.Extensions.Logging;
+
 
 namespace chatdict.webapi.Controllers;
 
@@ -13,35 +12,46 @@ public class TelHookController: ControllerBase
 {
 
     [HttpPost]
-    public async Task<string> OnTelUpdate([FromBody]Update update)
+    public async Task<string> OnTelUpdate([FromBody]Update? update)
     {
-        Console.WriteLine($"on tel webhook. {Request.ContentType} update id: {update.Id}");
-        var msg = update.Message;
-        if (msg == null)
+        Console.WriteLine($"on tel webhook. {Request.ContentType}");
+
+        if (update == null)
         {
-            Console.WriteLine("empty message");
-            return "empty msg";
+            Console.WriteLine($"update is null. content type {Request.ContentType}");
+            Request.Body.Seek(0, SeekOrigin.Begin);
+            var readResult = await Request.BodyReader.ReadAsync();
+            var body = Encoding.UTF8.GetString(readResult.Buffer);
+            Console.WriteLine($"request body: {body}");
+            return "empty update";
         }
         
-        Console.WriteLine($"message. type: {msg.Chat.Type}, chat id: {msg.Chat.Id} {msg.Chat.Username}, text: {msg.Text}");
-        if (string.IsNullOrEmpty(msg.Text))
-        {
-            Console.WriteLine("empty test");
-            return "empty text";
-        }
-
-        if (msg.Text.StartsWith("/"))
-        {
-            Console.WriteLine($"received tel command {msg.Text}");
-            return "command";
-        }
-
-        Console.WriteLine($"Update Hook {update.Id} {msg.Type} {msg.Text}");
-
-        var translateResult = await TranslateService.Default.Translate(text: msg.Text, "zh-CN");
-
-        var botClient = TelegramBotService.Default.Client;
-        var sentMsg = await botClient.SendTextMessageAsync(msg.Chat.Id, translateResult.TranslatedText);
+        // var msg = update.Message;
+        // if (msg == null)
+        // {
+        //     Console.WriteLine("empty message");
+        //     return "empty msg";
+        // }
+        //
+        // Console.WriteLine($"update id: {update.Id}.  chat type: {msg.Chat.Type}, chat id: {msg.Chat.Id} {msg.Chat.Username}, text: {msg.Text}");
+        // if (string.IsNullOrEmpty(msg.Text))
+        // {
+        //     Console.WriteLine("empty test");
+        //     return "empty text";
+        // }
+        //
+        // if (msg.Text.StartsWith("/"))
+        // {
+        //     Console.WriteLine($"received tel command {msg.Text}");
+        //     return "command";
+        // }
+        //
+        // Console.WriteLine($"Update Hook {update.Id} {msg.Type} {msg.Text}");
+        //
+        // var translateResult = await TranslateService.Default.Translate(text: msg.Text, "zh-CN");
+        //
+        // var botClient = TelegramBotService.Default.Client;
+        // var sentMsg = await botClient.SendTextMessageAsync(msg.Chat.Id, translateResult.TranslatedText);
 
         return "ok";
     }
